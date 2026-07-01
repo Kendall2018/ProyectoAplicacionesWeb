31805d291e618b8996898f1d35d94c73877ccdde
Kendall2018 <147559476+Kendall2018@users.noreply.github.com>
2026-07-01 13:57:28 -0600

    Configuracion BD

---
+++ ProyectoAplicacionesWeb/src/test/java/com/mycompany/ProyectoAplicacionesWeb/ProyectoAplicacionesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.ProyectoAplicacionesWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAplicacionesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
